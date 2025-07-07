67da1f630fc68705f612af7ca43ff51f8975cebb
TanJeeSchuan <89920999+TanJeeSchuan@users.noreply.github.com>
2025-07-07 12:06:43 +0800

    buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("This is a app bar"),
+          backgroundColor: Colors.amber,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Column(
+              children: [
+                Text('Hello World!'),
+                Text('Login'),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+
+              ],
+          ),
         ),
       ),
     );
